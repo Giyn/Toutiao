ca51e22213794bdbde960586bcbfd27e6aabde9b
“Giyn” <490601115@qq.com>
2022-06-09 21:53:18 +0800

    feat: 底部TabBar控制器

---
+++ Toutiao/TTTabBarController.h
@@ -0,0 +1,16 @@
+//
+//  TTTabBarController.h
+//  Toutiao
+//
+//  Created by Admin on 2022/6/9.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface TTTabBarController : UITabBarController
+
+@end
+
+NS_ASSUME_NONNULL_END
