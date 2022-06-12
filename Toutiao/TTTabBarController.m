8ef3e6a1894bb87d053b23bc9731cd8770bc1b71
aluo <2324105701@qq.com>
2022-06-12 09:35:51 +0800

    style:homepage添加导航栏

--- Toutiao/TTTabBarController.m
+++ Toutiao/TTTabBarController.m
@@ -31,6 +31,8 @@
 
     // 创建子控制器 - 主页
     UIViewController *vcHomePage = [[TTPagerViewController alloc] initWithChildrenVCArray:@[TTVideoStreamController.new, TTVideoStreamController.new, TTVideoStreamController.new] titles:@[@"第一页", @"第二页", @"第三页"]];
+    UINavigationController *navcHomePage = [[UINavigationController alloc] initWithRootViewController:vcHomePage];
+    vcHomePage.navigationController.navigationBar.hidden = YES;
     vcHomePage.tabBarItem.title = @"主页";
     vcHomePage.tabBarItem.image = [UIImage imageNamed:@"homepage"];
     UIImage *imageHomePage = [UIImage imageNamed:@"homepage_highlighted"];
@@ -58,7 +60,7 @@
     navMine.tabBarItem.selectedImage = imageMine;
     [navMine.tabBarItem setTitleTextAttributes:attrs forState:UIControlStateNormal];
     
-    [self addChildViewController:vcHomePage];
+    [self addChildViewController:navcHomePage];
     [self addChildViewController:vcUpload];
     [self addChildViewController:navMine];
     
